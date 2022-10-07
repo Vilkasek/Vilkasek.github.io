5a6a93c1c632b492637ebbca72d51756a2c20e06
Vilkasek <jakub.brandt2002@gmail.com>
2022-10-07 18:21:34 +0200

    Stworzenie funkcji zegara i zmiennych

---
+++ Scripts/clock.js
@@ -0,0 +1,10 @@
+let div = document.getElementById("clock");
+
+function clock()
+{
+    let date = new Date();
+
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+}
