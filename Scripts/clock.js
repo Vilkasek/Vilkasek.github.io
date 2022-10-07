5d825416b2badc501aed97f79b3d63e9b162d29e
Vilkasek <jakub.brandt2002@gmail.com>
2022-10-07 18:25:30 +0200

    Utworzenie funkcji dodającej zero do wartości < 10

--- Scripts/clock.js
+++ Scripts/clock.js
@@ -7,4 +7,14 @@ function clock()
     let h = date.getHours();
     let m = date.getMinutes();
     let s = date.getSeconds();
+
+    div.innerHTML = h + " : " + m + " : " + s;
+}
+
+function add_zero(x)
+{
+    if(x < 10)
+    {
+        div.innerHTML = "0" + x;
+    }
 }
