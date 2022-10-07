42ffa5edb10f2a99fe1f9bee2e400a291a8fa517
Vilkasek <jakub.brandt2002@gmail.com>
2022-10-07 18:55:51 +0200

    Ukończenie skryptu zegara

--- Scripts/clock.js
+++ Scripts/clock.js
@@ -8,17 +8,17 @@ function clock()
     let m = date.getMinutes();
     let s = date.getSeconds();
 
+    h = add_zero(h);
+    m = add_zero(m);
+    s = add_zero(s);
+
     div.innerHTML = h + " : " + m + " : " + s;
+
+    setTimeout(clock, 1000)
 }
 
 function add_zero(x)
 {
-    if(x < 10)
-    {
-        return "0" + x;
-    }
-    else
-    {
-        return x;
-    }
+    if(x < 10){ x = "0" + x};
+    return x;
 }
